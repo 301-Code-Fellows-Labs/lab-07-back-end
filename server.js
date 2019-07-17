b38ac36420e7e7ff316bd6c9fdd6043a8ace5b12
Nadya Ilinskaya <nadya.ilinskiy@gmail.com>
2019-07-17 10:03:13 -0700

    update server.js

--- server.js
+++ server.js
@@ -1,57 +1,66 @@
-'use strict'
+'use strict';
 
+// Load Environment Variables from the .env file
 require('dotenv').config();
+
+// Application Dependencies
 const express = require('express');
-const app = express();
 const cors = require('cors');
+const superagent = require('superagent');
 
+// Application Setup
 const PORT = process.env.PORT || 3000;
-
+const app = express();
 app.use(cors());
 
-let locations = [];
-let weatherAtLocation = [];
+// API Routes
 
-app.get('/location', (request, response) => {
-  try {
-    const geoData = require('./data/geo.json');
-    const location = new Location(request.query.data, geoData);
-    locations.push(location);
-    response.send(locations);
-  } catch (error) {
-    response.status(400).send({ '404 error': error });
-  }
-});
+app.get('/location', searchToLatLong);
 
+app.get('/weather', getWeather);
 
-app.get('/weather', (request, response) => {
-  try{
-    const weatherData = require('./data/darksky.json');
-    const weather = new Weather(request.query.data, weatherData);
-    response.send(weather);
-  } catch (error) {
-    response.status(400).send({ '404 error': error });
-  }
-});
+// Helper Functions
 
-function Location(query, geoData) {
-  this.search_query = query;
-  this.latitude = geoData.results[0].geometry.location.lat;
-  this.longitude = geoData.results[0].geometry.location.lat;
-  this.name = geoData.results[0].formatted_address;
-}
-function Weather(query, weatherData) {
-  this.search_query = query;
-//   this.latitude = weatherData.latitude;
-//   this.longitude = weatherData.longitude;
-  
-    this.weatherAtLocation = weatherData.hourly.data.forEach(element => {
-    this.forecast = element.summary;
-    this.time = new Date(element.time).toString();  
-  });
-  
+function searchToLatLong(request, response) {
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.data}&key=${process.env.GEOCODE_API_KEY}`
+
+  return superagent.get(url)
+    .then(res => {
+      const location = new Location(request.query.data, JSON.parse(res.text));
+      response.send(location);
+    })
+    .catch(err => {
+      response.send(err);
+    });
 }
 
-app.listen(PORT, () => {
-  console.log('Listening on port: ' + PORT);
-})
+function Location(query, res) {
+  this.search_query = query;
+  this.formatted_query = res.results[0].formatted_address;
+  this.latitude = res.results[0].geometry.location.lat;
+  this.longitude = res.results[0].geometry.location.lng;
+}
+
+function getWeather(request, response) {
+  const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`
+
+  return superagent.get(url)
+    .then(res => {
+      const weatherEntries = res.body.daily.data.map(day => {
+        return new Weather(day);
+      })
+
+      response.send(weatherEntries);
+    })
+    .catch(error => {
+      response.send(error);
+    });
+}
+
+function Weather(day) {
+  this.forecast = day.summary;
+  this.time = new Date(day.time * 1000).toString().slice(0, 15);
+}
+
+// Make sure the server is listening for requests
+app.listen(PORT, () => console.log(`App is up on ${PORT}`));
